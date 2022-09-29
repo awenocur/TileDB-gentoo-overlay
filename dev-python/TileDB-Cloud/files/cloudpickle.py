15e5f3a37d4fead27742c2b0f001e2932547dec0
Adam Wenocur <awenocur@gmail.com>
2022-09-29 02:19:45 -0400

    mitigate cloudpickle side-effects

--- dev-python/TileDB-Cloud/files/cloudpickle.py
+++ dev-python/TileDB-Cloud/files/cloudpickle.py
@@ -1,6 +1,13 @@
 import sys
-import tiledb.cloudpickle
+old_module = sys.modules['cloudpickle'] if sys.modules.__contains__('cloudpickle') else None
+import tiledb.cloudpickle as internal_cloudpickle
 sys.modules['cloudpickle'] = sys.modules['tiledb.cloudpickle']
 sys.modules['cloudpickle'].__name__ = 'cloudpickle'
+del internal_cloudpickle
 sys.modules.pop('tiledb.cloudpickle')
 from cloudpickle import *
+sys.modules.pop('cloudpickle')
+if old_module:
+    sys.modules['cloudpickle'] = old_module
+del old_module
+del sys
