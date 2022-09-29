4d37d5bce0155df149127fc649e4ac7aaa4ce67b
Adam Wenocur <awenocur@gmail.com>
2022-09-29 03:56:25 -0400

    TileDB-Cloud should override system cloudpickle

--- dev-python/TileDB-Cloud/files/cloudpickle.py
+++ dev-python/TileDB-Cloud/files/cloudpickle.py
@@ -1,13 +1,11 @@
 import sys
-old_module = sys.modules['cloudpickle'] if sys.modules.__contains__('cloudpickle') else None
 import tiledb.cloudpickle as internal_cloudpickle
 sys.modules['cloudpickle'] = sys.modules['tiledb.cloudpickle']
 sys.modules['cloudpickle'].__name__ = 'cloudpickle'
 del internal_cloudpickle
+import importlib
+importlib.reload(sys.modules['cloudpickle'])
+del importlib
 sys.modules.pop('tiledb.cloudpickle')
 from cloudpickle import *
-sys.modules.pop('cloudpickle')
-if old_module:
-    sys.modules['cloudpickle'] = old_module
-del old_module
 del sys
